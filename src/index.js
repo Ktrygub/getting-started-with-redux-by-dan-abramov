f82c444134350dff9a2f0a2c16c9a998a16a25aa
Ktrygub <konstantin.trygub@gmail.com>
2017-11-24 15:07:49 +0200

    React Todo List Example

--- src/index.js
+++ src/index.js
@@ -1,43 +1,153 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import PropTypes from 'prop-types'
 
-const counter = (state = 0, action) => {
+const todo = (state, action) => {
   switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
+    case 'ADD_TODO':
+      return { id: action.id, text: action.text, isCompleted: false }
+    case 'TOGGLE_TODO':
+      if (state.id !== action.id) {
+        return state
+      }
+      return { ...state, isCompleted: !state.isCompleted }
+    default:
+      return state
+  }
+}
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, todo(undefined, action)]
+    case 'TOGGLE_TODO':
+      return state.map(t => todo(t, action))
     default:
       return state
   }
 }
 
-const store = createStore(counter)
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SHOW_ALL':
+    case 'SHOW_ACTIVE':
+    case 'SHOW_COMPLETED':
+      return action.type
+    default:
+      return state
+  }
+}
 
-const Counter = ({ value, onIncrement, onDecrement }) => (
-  <div>
-    <h1>{value}</h1>
-    <button onClick={onDecrement}>-</button>
-    <button onClick={onIncrement}>+</button>
-  </div>
-)
-Counter.propTypes = {
-  value: PropTypes.number.isRequired,
-  onIncrement: PropTypes.func.isRequired,
-  onDecrement: PropTypes.func.isRequired
+const reducer = combineReducers({ todos, visibilityFilter })
+const store = createStore(reducer)
+
+const FilterLink = ({ filter, children, currentFilter }) => {
+  if (filter === currentFilter) {
+    return <span>{children}</span>
+  }
+  return (
+    <a
+      href="/"
+      onClick={e => {
+        e.preventDefault()
+        store.dispatch({ type: filter })
+      }}
+    >
+      {children}
+    </a>
+  )
+}
+FilterLink.propTypes = {
+  filter: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
+  currentFilter: PropTypes.string.isRequired
+}
+
+const getVisibleTodos = (todos, visibilityFilter) => {
+  switch (visibilityFilter) {
+    case 'SHOW_ALL':
+      return todos
+    case 'SHOW_ACTIVE':
+      return todos.filter(todo => !todo.isCompleted)
+    case 'SHOW_COMPLETED':
+      return todos.filter(todo => todo.isCompleted)
+    default:
+      return []
+  }
+}
+
+class TodoApp extends React.Component {
+  render() {
+    const { todos, visibilityFilter } = this.props.store
+    const visibleTodos = getVisibleTodos(todos, visibilityFilter)
+    return (
+      <div>
+        <input
+          ref={input => {
+            this.input = input
+          }}
+        />
+
+        <button
+          onClick={() => {
+            store.dispatch({
+              type: 'ADD_TODO',
+              /* id: todos.length, */
+              id: todos.length > 0 ? todos[todos.length - 1].id + 1 : 0,
+              text: this.input.value
+            })
+            this.input.value = ''
+          }}
+        >
+          +
+        </button>
+
+        <ul>
+          {visibleTodos.map(todo => (
+            <li
+              key={todo.id}
+              role="presentation"
+              onClick={() => {
+                store.dispatch({ type: 'TOGGLE_TODO', id: todo.id })
+              }}
+              style={{ textDecoration: todo.isCompleted ? 'line-through' : 'none' }}
+            >
+              {todo.text}
+            </li>
+          ))}
+        </ul>
+
+        <p>
+          Show:{' '}
+          <FilterLink filter="SHOW_ALL" currentFilter={visibilityFilter}>
+            all
+          </FilterLink>{' '}
+          <FilterLink filter="SHOW_ACTIVE" currentFilter={visibilityFilter}>
+            active
+          </FilterLink>{' '}
+          <FilterLink filter="SHOW_COMPLETED" currentFilter={visibilityFilter}>
+            completed
+          </FilterLink>
+        </p>
+      </div>
+    )
+  }
+}
+TodoApp.propTypes = {
+  store: PropTypes.shape({
+    todos: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        text: PropTypes.string.isRequired,
+        isCompleted: PropTypes.bool.isRequired
+      }).isRequired
+    ).isRequired,
+    visibilityFilter: PropTypes.string.isRequired
+  }).isRequired
 }
 
 const render = () => {
-  ReactDOM.render(
-    <Counter
-      value={store.getState()}
-      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-    />,
-    document.getElementById('root')
-  )
+  ReactDOM.render(<TodoApp store={store.getState()} />, document.getElementById('root'))
 }
 
 render()
